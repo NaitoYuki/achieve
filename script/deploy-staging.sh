12c136490b6af62e42b06454b3b62a91fb2f4edc
Yuki <flower127834.00@gmail.com>
2017-07-17 05:35:58 +0000

    setup deploy AWS

--- script/deploy-staging.sh
+++ script/deploy-staging.sh
@@ -2,7 +2,7 @@
 
 export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
 export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
-export AWS_DEFAULT_REGION="ap-northeast-1a"
+export AWS_DEFAULT_REGION="ap-northeast-1"
 
 MYSECURITYGROUP="sg-32853a54"
 MYIP=`curl -s ifconfig.me`
