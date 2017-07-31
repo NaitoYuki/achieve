ca21001d2bb7b21994e1da83b112cd9dae233403
Yuki <flower127834.00@gmail.com>
2017-07-31 07:28:31 +0000

    test

---
+++ script/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+export AWS_DEFAULT_REGION="ap-northeast-1"
+
+if [ $1 -eq "staging" ]; then
+  MYSECURITYGROUP="sg-32853a54"
+elif [ $1 -eq "production" ]; then
+  MYSECURITYGROUP="*"
+fi
+
+MYIP=`curl -s ifconfig.me`
+
+aws ec2 authorize-security-group-ingress --group-id $MYSECURITYGROUP --protocol tcp --port 22 --cidr $MYIP/32
+
+bundle exec cap production deploy
+
+aws ec2 revoke-security-group-ingress --group-id $MYSECURITYGROUP --protocol tcp --port 22 --cidr $MYIP/32
